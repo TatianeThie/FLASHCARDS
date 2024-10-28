54f50e4bf415ce5851e8283579366a398e042e65
TatianeThie <168384729+TatianeThie@users.noreply.github.com>
2024-10-28 17:46:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'PROGRAMAÇÃO',
+    'O QUE É PYTHON?',
+    'O PYTHON É UMA LINGUAGEM DE PROGRAMAÇÃO'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'QUAL A CAPITAL DA FRANÇA?',
+    'A CAPITAL DE PARIS É FRANÇA'
+)
+
+criaCartao(
+    'PROGRAMAÇÃO',
+    'O QUE É UMA FUNÇÃO?',
+    'UMA FUNÇÃO É UM BLOCO DE CÓDIGO QUE EXECUTA ALGUMA TAREFA'
+)
+
+criaCartao(
+    'LÍNGUA INGLESA',
+    'COMO SE DIZ OI EM INGLÊS?',
+    'OI EM INGLÊs É HI (RAI)'
+) 
